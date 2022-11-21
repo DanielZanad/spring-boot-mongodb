b298a25025ca407f5db533c45251b6f8f766dfca
DanielZanad <daniel.zanad.dz@gmail.com>
2022-11-21 17:43:00 -0300

    UserService, UserResource add with error treatment

--- src/main/java/com/example/workshopmongo/services/UserService.java
+++ src/main/java/com/example/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.example.workshopmongo.services;
 
 import com.example.workshopmongo.domain.User;
 import com.example.workshopmongo.repository.UserRepository;
+import com.example.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,8 +15,19 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repo.findById(id);
+
+        if (user.isPresent()) {
+            return user.get();
+        }
+
+        throw new ObjectNotFoundException("Object not found");
+
     }
 
 }
